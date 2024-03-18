9cc1b42e7a7470fd4e3f9ed74358e09f7cc76716
Eric Z. Ayers <ericzundel@gmail.com>
2024-03-18 08:37:40 -0400

    Turn on the led strip example code by default

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 # Shell program to select one of the code files
-MODE="default"
-#MODE="steamworkshop-example"
+#MODE="default"
+MODE="steamworkshop-example"
 
 with open("code-"+MODE+".py") as f:
     exec(f.read())
